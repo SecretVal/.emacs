58add12ab1cb8346d55edb36ff0fb4f5f8f80dd6
secretval <108518296+SecretVal@users.noreply.github.com>
2025-07-21 16:09:15 +0200

    fix(evil): evil-want-keybinding nil

--- config.el
+++ config.el
@@ -19,6 +19,7 @@
 (use-package evil
   :config
   (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+  (setq evil-want-keybinding nil)
   :init (evil-mode 1))
 (use-package evil-collection
   :after evil
